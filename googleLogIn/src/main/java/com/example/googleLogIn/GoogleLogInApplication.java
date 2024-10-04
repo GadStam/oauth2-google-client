5d82d62d509ceaef6ea409029dfd6ad276a6bae3
gadstamati <gadstam71@gmail.com>
2024-10-04 17:41:11 -0300

    waiting for update

---
+++ googleLogIn/src/main/java/com/example/googleLogIn/GoogleLogInApplication.java
@@ -0,0 +1,13 @@
+package com.example.googleLogIn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleLogInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleLogInApplication.class, args);
+	}
+
+}
